be73d3faa8f371516250798169eb7446968af144
alexcl01 <acastellanol1@alumnos.nebrija.es>
2022-10-14 17:34:03 +0200

    Clase Creación API

--- main.ts
+++ main.ts
@@ -1,60 +1,36 @@
-type Info = {
-  count: number,
-  pages: number,
-  next: string | null,
-  prev: string | null,
-}
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
-type Character = {
-  id: number,
-  name: string,
-  status: string,
-  species: string,
-  type: string,
-  gender: string,
-  origin: Location,
-  location: Location,
-  image: string,
-  episode: string[],
-  url: string,
-  created: string,
-} 
+const app = new Application();
 
-type CharactersData = {
-  info: Info,
-  results: Character[],
-}
+const router = new Router();
 
-
-const response = await fetch("https://rickandmortyapi.com/api/character/?name=rick&status=alive");
-
-const data: CharactersData = await response.json();
-
-const rick = data.results.map(char => {
-  return {
-    name: char.name,
-    status: char.status,
-    episode: char.episode,
+router.get('/', (ctx) => {
+  ctx.response.body = "Hola majo";
+}).get("/day", (ctx) => {
+  ctx.response.body = {
+    day: new Date().getDay(),
   };
-});
+})
+  .get("/characters", async (ctx) => {
+    const data = await fetch("https://rickandmortyapi.com/api/character");
+    const json = await data.json();
+    ctx.response.body = json;
+  })
+  .get("/characters/:page", async (ctx) => {
+    const page = ctx.params?.page;
 
-let next = data.info.next;
+    if(Number(page) < 1 || Number(page) > 42) {
+      ctx.response.status = 404;
+      ctx.response.body = "Numero incorrecto";
+      return;
+    }
 
-while (next) {
-  const response = await fetch("https://rickandmortyapi.com/api/character/?name=rick&status=alive");
-
-  const data: CharactersData = await response.json();
-
-  const allRicks = data.results.map(char => {
-    return {
-      name: char.name,
-      status: char.status,
-      episode: char.episode,
-    };
+    const data = await fetch(`https//rickandmortyapi.com/api/character?page=${page}`);
+    const json = await data.json();
+    ctx.response.body = json;
   });
-  rick.push(...allRicks);
-  next = data.info.next;
-  
-}
 
-console.log(rick);
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
